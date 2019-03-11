151e5de44602798100a47d51f3feb0f17b04bf24
AnnaTuzova <anntuz00@gmail.com>
2019-03-11 17:07:48 +0300

    Second commit
    Исправлены недочеты, дописаны тесты

--- Sort/Sort/Sort.cpp
+++ Sort/Sort/Sort.cpp
@@ -5,108 +5,126 @@
 #include <ctime>
 #include <algorithm>
 
-const int kNumOfElements = 100;
+const int kNumOfElementsGlobal = 100;
 const char* kPath = "file.txt";
-void FillingFile(int num_of_elements = kNumOfElements); 
-int *ReadingFile(int num_of_elements = kNumOfElements); 
-int *SelectionSort(int *data, int num_of_elements = kNumOfElements); 
-void WritingFile(int *sorted_data, int num_of_elements = kNumOfElements); 
-void CheckSort(int *test_array, int *test_sorted_data, int num_of_elements = kNumOfElements);
-void FirstTest(int num_of_elements = kNumOfElements); 
-void SecondTest(int num_of_elements = kNumOfElements); 
+void FillingFile(const int kNumOfElements = kNumOfElementsGlobal);
+void ReadingFile(int *data, const int kNumOfElements = kNumOfElementsGlobal);
+void SelectionSort(int *data, const int kNumOfElements = kNumOfElementsGlobal);
+void WritingFile(int *sorted_data, const int kNumOfElements = kNumOfElementsGlobal);
+void CheckSort(int *test_array, int *test_sorted_data, const int kNumOfElements = kNumOfElementsGlobal);
+void TestRanomdArray(const int kNumOfElements = kNumOfElementsGlobal);
+void TestSortedArray(const int kNumOfElements = kNumOfElementsGlobal);
+void TestOneMinusOneArray(const int kNumOfElements = kNumOfElementsGlobal);
+void TestEmptyArray();
+void TestReverseOrderSortedArray(const int kNumOfElements = kNumOfElementsGlobal);
 
 int main()
 {
-	FillingFile();
-	int *data = ReadingFile();
-	int *sorted_data = SelectionSort(data);
-	WritingFile(sorted_data);
-	FirstTest();
-	SecondTest();
+	int *data = new int[kNumOfElementsGlobal];
 	
+	FillingFile();
+	ReadingFile(data);
+	SelectionSort(data);
+	WritingFile(data);
+	TestRanomdArray();
+	TestSortedArray();
+	TestOneMinusOneArray();
+	TestEmptyArray();
+	TestReverseOrderSortedArray();
+
+	delete[] data;
 	return 0;
 }
 
-void FillingFile(int num_of_elements)
+void FillingFile(const int kNumOfElements)
 {
 	std::ofstream file(kPath, std::ios_base::out | std::ios_base::trunc); 
 	if (!file.is_open())
 	{
 		std::cout << "File is not open for filling!" << std::endl;
 	}
+
 	std::srand(time(0)); 
-	for (int i = 0; i < num_of_elements; i++) 
+	for (int i = 0; i < kNumOfElements; i++) 
 	{
 		file << std::rand() % 1000 << std::endl;
 	}
+
 	file.close();
 }
 
-int *ReadingFile(int num_of_elements)
+void ReadingFile(int *data, const int kNumOfElements)
 {
 	std::ifstream file(kPath);
 	if (!file.is_open()) 
 	{
 		std::cout << "File is not open for reading!" << std::endl;
 	}
+
 	file.seekg(0, std::ios_base::end); 
 	std::cout << "Size of file: " << file.tellg() << " bytes" << std::endl;
 	file.seekg(0, std::ios_base::beg); 
-	int *buffer = new int[num_of_elements]; 
-	for (int i = 0; i < num_of_elements; i++) 
+
+	for (int i = 0; i < kNumOfElements; i++) 
 	{
-		file >> buffer[i];
+		file >> data[i];
 	}
+
 	file.close();
-	return buffer; 
 }
 
 
-int *SelectionSort(int *data, int num_of_elements)
+void SelectionSort(int *data, const int kNumOfElements)
 {
-	for (int i = 0; i < num_of_elements - 1; i++)
+
+	for (int i = 0; i < kNumOfElements - 1; i++)
 	{
-		int min_ind = i;
-		for (int j = i + 1; j < num_of_elements; j++)
+		int min_index = i;
+		for (int j = i + 1; j < kNumOfElements; j++)
 		{
-			if (data[min_ind] > data[j])
+			if (data[min_index] > data[j])
 			{
-				min_ind = j;
+				min_index = j;
 			}
 
 		}
-		if (min_ind != i)
+
+		if (min_index != i)
 		{
-			std::swap(data[i], data[min_ind]);
+			std::swap(data[i], data[min_index]);
 		}
 	}
-	return data;
 }
 
-void WritingFile(int *sorted_data, int num_of_elements)
+void WritingFile(int *sorted_data, const int kNumOfElements)
 {
 	std::ofstream file(kPath, std::ios_base::app);
 	if (!file.is_open()) 
 	{
 		std::cout << "File is not open for writing!" << std::endl;
 	}
+
 	file << "Sorted data: " << std::endl;
-	for (int i = 0; i < num_of_elements; i++)
+
+	for (int i = 0; i < kNumOfElements; i++)
 	{
 		file << sorted_data[i] << std::endl;
 	}
+
 	file.close();
 }
 
-void CheckSort(int *test_array, int *test_sorted_data, int num_of_elements)
+void CheckSort(int *test_array, int *test_sorted_data, const int kNumOfElements)
 {
-	int *check = new int[num_of_elements];
+	int *check = new int[kNumOfElements];
 	int check_sum = 0;
-	for (int i = 0; i < num_of_elements; i++)
+	
+	for (int i = 0; i < kNumOfElements; i++)
 	{
 		check[i] = test_array[i] - test_sorted_data[i];
 		check_sum = check_sum + check[i];
 	}
+
 	if (check_sum == 0)
 	{
 		std::cout << "Check is passed. Sorting works correctly. \n";
@@ -115,33 +133,103 @@ void CheckSort(int *test_array, int *test_sorted_data, int num_of_elements)
 	{
 		std::cout << "Check is failed. Sorting does not work correctly. \n";
 	}
+
 	delete[] check;
 }
 
-void FirstTest(int num_of_elements)
+void TestRanomdArray(const int kNumOfElements)
 {
-	int *test_array = new int[num_of_elements];
+	int *actual_array = new int[kNumOfElements];
+	int *expected_array = new int[kNumOfElements];
+
 	std::srand(time(0));
-	for (int i = 0; i < num_of_elements; i++)
+	for (int i = 0; i < kNumOfElements; i++)
 	{
-		test_array[i] = std::rand() % 1000;
+		actual_array[i] = std::rand() % 1000;
 	}
-	std::sort(test_array, test_array + num_of_elements);
-	int *test_sorted_data = SelectionSort(test_array, num_of_elements);
+
+	std::copy(actual_array, actual_array + kNumOfElements, expected_array);
+	std::sort(actual_array, actual_array + kNumOfElements);
+
+	SelectionSort(expected_array);
 	std::cout << "Check number one: comparing selection sorting with standard sorting. \n";
-	CheckSort(test_array, test_sorted_data);
-	delete[] test_array;
+	CheckSort(actual_array, expected_array);
+
+	delete[] actual_array, expected_array;
 }
 
-void SecondTest(int num_of_elements)
+void TestSortedArray(const int kNumOfElements)
 {
-	int *test_array = new int[num_of_elements];
-	for (int i = 0; i < num_of_elements; i++)
+	int *actual_array = new int[kNumOfElements];
+	int *expected_array = new int[kNumOfElements];
+
+	for (int i = 0; i < kNumOfElements; i++)
 	{
-		test_array[i] = i;
+		actual_array[i] = i;
 	}
-	int *test_sorted_data = SelectionSort(test_array);
+	std::copy(actual_array, actual_array + kNumOfElements, expected_array);
+
+	SelectionSort(expected_array);
 	std::cout << "Check number two: comparing selection sorting with sorted array. \n";
-	CheckSort(test_array, test_sorted_data);
-	delete[] test_array;
+	CheckSort(actual_array, expected_array);
+
+	delete[] actual_array, expected_array;
+}
+
+void TestOneMinusOneArray(const int kNumOfElements)
+{
+	int *actual_array = new int[kNumOfElements];
+	int *expected_array = new int[kNumOfElements];
+
+	std::srand(time(0));
+	for (int i = 0; i < kNumOfElements; i++)
+	{
+		actual_array[i] = std::rand() % 2;
+
+		if (actual_array[i] == 0)
+		{
+			actual_array[i] = -1;
+		}
+	}
+
+	std::copy(actual_array, actual_array + kNumOfElements, expected_array);
+	std::sort(actual_array, actual_array + kNumOfElements);
+	SelectionSort(expected_array);
+
+	std::cout << "Check number three: comparing selection sorting with array of 1 and -1. \n";
+	CheckSort(actual_array, expected_array);
+
+	delete[] actual_array, expected_array;
+}
+
+void TestEmptyArray()
+{
+	int* zero_pointer = 0;
+	SelectionSort(zero_pointer, 0);
+	if (zero_pointer == 0)
+	{
+		std::cout << "Check number four: sorting empty array. \n" << "Passing an empty array returns an empty array. Sorting works correctly. \n";
+	}
+	else
+	{
+		std::cout << "Check is failed. Sorting does not work correctly. \n";
+	}
+}
+
+void TestReverseOrderSortedArray(const int kNumOfElements)
+{
+	int *actual_array = new int[kNumOfElements];
+	int *expected_array = new int[kNumOfElements];
+
+	for (int i = 0; i < kNumOfElements; i++)
+	{
+		actual_array[i] = (kNumOfElements - 1) - i;
+	}
+
+	std::copy(actual_array, actual_array + kNumOfElements, expected_array);
+	SelectionSort(expected_array);
+	std::cout << "Check number five: comparing selection sorting with array that sorted in reverse order. \n";
+	CheckSort(actual_array, expected_array);
+
+	delete[] actual_array, expected_array;
 }
